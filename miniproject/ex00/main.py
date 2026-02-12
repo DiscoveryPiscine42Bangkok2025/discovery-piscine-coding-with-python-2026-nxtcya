29b2476ace88289f365ad4d903ee195562d4ab7d
nxtcya <quinn3nxt@gmail.com>
2026-02-13 01:54:35 +0700

    miniproject

---
+++ miniproject/ex00/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from checkmate import  condition_game
+
+def main(): 
+
+    board_str = """\
+    R...
+    .K..
+    ..P.
+    ...P\
+"""
+   
+
+    condition_game(board_str)
+
+if __name__ == "__main__":
+    main()
